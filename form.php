aa902c5d80290d053f848df8533e811884e52b3c
Ciugy <91850238+Ciugy@users.noreply.github.com>
2025-09-24 12:30:44 -0400

    Part3d - Lab 5

--- form.php
+++ form.php
@@ -1,11 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
 <body>
-    <h1>User Agent</h1>
+    <h1>Who are you?</h1>
  
     <p> 
         Welcome <?php echo $_GET["name"] . " " . $_GET["last-name"]; ?><br>
         Your email address is: <?php echo $_GET["email"]; ?>
+        GET: <?= var_dump($_GET) ?><br>
+        POST: <?= var_dump($_POST) ?>
+
     </p>
 
 </body>
