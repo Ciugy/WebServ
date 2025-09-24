04c09fad840821e11f4e623e571aeac13751ec57
Ciugy <91850238+Ciugy@users.noreply.github.com>
2025-09-24 11:59:20 -0400

    Part 2b - Lab 5

--- response.php
+++ response.php
@@ -5,9 +5,9 @@
     <title>Simple HTML Template</title>
 </head>
 <body>
-    <h1>Welcome to My Simple HTML Page</h1>
+    <h1>User Agent</h1>
     <p>    
-        <?=
+        <?php
         $userAgent = $_SERVER['HTTP_USER_AGENT'];
         echo "User-Agent: " . $userAgent;
         ?>
