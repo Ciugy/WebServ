c166174d86029f052586b66c0c59652afb266911
Ciugy <91850238+Ciugy@users.noreply.github.com>
2025-09-24 11:33:31 -0400

    Part 1 - Lab 5

---
+++ response.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Simple HTML Template</title>
+</head>
+<body>
+    <h1>Welcome to My Simple HTML Page</h1>
+    <p>This is a basic HTML template.</p>
+    <p><?= var_dump($_SERVER) ?></p>
+</body>
+</html>
