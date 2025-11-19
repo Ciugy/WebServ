0a45ce0971b299244fdbe57cb1157d93ba0de790
Ciugy <91850238+Ciugy@users.noreply.github.com>
2025-11-19 12:11:34 -0500

    Button7

--- toggle.php
+++ toggle.php
@@ -2,14 +2,13 @@
 // toggle.php
 
 // BCM GPIO number for physical pin 7
-$gpioPin = 7;
+$gpioPin = 4;  // pin 7 = BCM 4
 
 if (!isset($_POST['state'])) {
     die("No state specified.");
 }
 
 $state = $_POST['state'] === 'on' ? '1' : '0';
-$state = $_POST['state'] === 'off' ? '1' : '0';
 
 // Set pin mode to output
 shell_exec("sudo gpio -g mode {$gpioPin} out");
@@ -17,6 +16,9 @@ shell_exec("sudo gpio -g mode {$gpioPin} out");
 // Write the value
 shell_exec("sudo gpio -g write {$gpioPin} {$state}");
 
-// Redirect back to the button page
+// Save current state to a temp file (for display on index.php)
+file_put_contents("/tmp/led_state.txt", $state);
+
+// Redirect back to the main page
 header("Location: clickbutton.php");
 exit;
