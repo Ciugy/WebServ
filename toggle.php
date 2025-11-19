1f254e40db666e0324b6d7bc94b1df904443c521
Ciugy <91850238+Ciugy@users.noreply.github.com>
2025-11-19 12:55:45 -0500

    Button22

--- toggle.php
+++ toggle.php
@@ -3,19 +3,13 @@
 
 $gpioPin = 7; 
 
-if ($state = $_POST['state'] === 'on') {
-    $state = '1';
+if (isset($_POST['state']) && $_POST['state'] === 'on') {
+    $state = 1;
+    $output = shell_exec("sudo gpio write {$gpioPin} 1");
 } else {
-    $state = '0';
+    $state = 0;
+    $output = shell_exec("sudo gpio write {$gpioPin} 0");
 }
-
-
-// Set pin mode to output
-shell_exec("sudo gpio mode {$gpioPin} out");
-
-// Write the value (turn LED ON or OFF)s
-shell_exec("sudo gpio write {$gpioPin} {$state}");
-
 
 // Redirect back to the main page
 header("Location: clickbutton.php");
