ec9de2a97a45f87db9bcd23a224257f6e888bcb6
Ciugy <91850238+Ciugy@users.noreply.github.com>
2025-11-19 12:41:39 -0500

    Button18

--- toggle.php
+++ toggle.php
@@ -12,8 +12,8 @@ $state = $_POST['state'] === 'on' ? '1' : '0';
 // Set pin mode to output
 shell_exec("sudo gpio mode {$gpioPin} out");
 
-// Write the value (turn LED ON or OFF)
-shell_exec("sudo gpio toggle {$gpioPin}");
+// Write the value (turn LED ON or OFF)s
+shell_exec("sudo gpio write {$gpioPin} {$state}");
 
 
 // Redirect back to the main page
