fb09c1df668fe702f663a820893111d253d5a420
Ciugy <91850238+Ciugy@users.noreply.github.com>
2025-11-19 12:10:15 -0500

    Button6

--- toggle.php
+++ toggle.php
@@ -2,13 +2,14 @@
 // toggle.php
 
 // BCM GPIO number for physical pin 7
-$gpioPin = 7;  // pin 7 = BCM 4
+$gpioPin = 7;
 
 if (!isset($_POST['state'])) {
     die("No state specified.");
 }
 
 $state = $_POST['state'] === 'on' ? '1' : '0';
+$state = $_POST['state'] === 'off' ? '1' : '0';
 
 // Set pin mode to output
 shell_exec("sudo gpio -g mode {$gpioPin} out");
